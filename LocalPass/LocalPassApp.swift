daad4041cf967cfda88a199fb784374e894267e4
Reu7en <39139355+Reu7en@users.noreply.github.com>
2025-02-18 23:45:10 +0000

    Initial Commit

---
+++ LocalPass/LocalPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalPassApp.swift
+//  LocalPass
+//
+//  Created by Reuben on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LocalPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalPassTests/LocalPassTests.swift
@@ -0,0 +1,16 @@
+//
+//  LocalPassTests.swift
+//  LocalPassTests
+//
+//  Created by Reuben on 18/02/2025.
+//
+
+import Testing
+
+struct LocalPassTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
